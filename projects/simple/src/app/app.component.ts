e6e12ce691235920d7e0bd1b50ac8c6d73c0bffa
Mohammad H. Sattarian <mh_sattarian@outlook.com>
2020-07-27 14:45:48 +0430

    Update simple project example

--- projects/simple/src/app/app.component.ts
+++ projects/simple/src/app/app.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'mapir-angular-test';
-  center:Array<number> = [51.367918, 35.712706];
+  title = "mapir-angular-test";
+  center: Array<number> = [51.367918, 35.712706];
   markerPosition: Array<number> = [51.367918, 35.712706];
-  apiKey:string = "<YOUR API KEY>";
+  apiKey: string = "<YOUR API KEY>";
 
-  clicked(e: any){
-    if ('lngLat' in e) this.markerPosition = [e.lngLat.lng, e.lngLat.lat];
+  clicked(e: any) {
+    if ("lngLat" in e) this.markerPosition = [e.lngLat.lng, e.lngLat.lat];
+  }
+
+  dragged(e: any) {
+    console.log(e._lngLat);
   }
 }
